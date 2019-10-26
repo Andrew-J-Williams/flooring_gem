bca878fd7e2900a0f463c0ce576f439f2b6ce692
Andrew Williams <1williamsaj@gmail.com>
2019-10-26 19:29:47 +0000

    update desc

---
+++ lib/flooring_gem.rb
@@ -0,0 +1,6 @@
+require "flooring_gem/version"
+
+module FlooringGem
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/flooring_gem/version.rb
@@ -0,0 +1,3 @@
+module FlooringGem
+  VERSION = "0.1.0"
+end
