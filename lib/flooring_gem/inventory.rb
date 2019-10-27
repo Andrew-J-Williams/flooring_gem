be6de59c5ae19547c1a1e7d06d6c731a129e6b32
Andrew Williams <1williamsaj@gmail.com>
2019-10-27 21:43:40 +0000

    finalized hash

--- lib/flooring_gem/inventory.rb
+++ lib/flooring_gem/inventory.rb
@@ -1,24 +1,30 @@
 class FlooringGem::Inventory
 
-  attr_reader :name
+  attr_reader :name, :products
 
   @@all = {}
+  @@title = {}
 
   def initialize
     @name = FlooringGem::Scraper.new.city_names
+    @products = FlooringGem::Scraper.new.products
   end
 
   def create_records
     @name.each do |name|
-      @@all[name] = "test"
+      @@all[name] = self.all_products
     end
-
-
-    #@@all[@name[0]] = "empty"
-    #@@all[@name[1]] = "testing"
-    #@@all[@name[2]] = "this"
-
     @@all
   end
 
+  def all
+    @@all
+  end
+
+  def all_products
+    @products.each do |name|
+      @@title[name] = rand(10..1000).to_s + " sqft"
+    end
+    @@title
+  end
 end
