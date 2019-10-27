99dc09fb8b2b090336cc6e76bbdb5f2debd1444a
Andrew Williams <1williamsaj@gmail.com>
2019-10-27 19:46:43 +0000

    testing hashes

--- lib/flooring_gem/inventory.rb
+++ lib/flooring_gem/inventory.rb
@@ -1,7 +1,24 @@
 class FlooringGem::Inventory
 
+  attr_reader :name
+
+  @@all = {}
+
   def initialize
-    
+    @name = FlooringGem::Scraper.new.city_names
+  end
+
+  def create_records
+    @name.each do |name|
+      @@all[name] = "test"
+    end
+
+
+    #@@all[@name[0]] = "empty"
+    #@@all[@name[1]] = "testing"
+    #@@all[@name[2]] = "this"
+
+    @@all
   end
 
 end
