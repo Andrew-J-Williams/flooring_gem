ec24a857cbd0c1a6855595f2974f805d86cd436c
Andrew Williams <1williamsaj@gmail.com>
2019-10-27 20:24:31 +0000

    products method added

--- lib/flooring_gem/scraper.rb
+++ lib/flooring_gem/scraper.rb
@@ -1,19 +1,20 @@
 class FlooringGem::Scraper
 
-  attr_reader :site
+  attr_reader :floor_site, :product_site
 
   def initialize
-    @site = "https://themasterscraft.com"
+    @floor_site = "https://themasterscraft.com"
+    @product_site = "https://www.lumberliquidators.com/ll/s/hardwood-flooring"
   end
 
   def main_site
-    doc = Nokogiri::HTML(open(site))
+    doc = Nokogiri::HTML(open(floor_site))
     puts doc
   end
 
   def city_names
     city_names = []
-    doc = Nokogiri::HTML(open(site + "/contact/"))
+    doc = Nokogiri::HTML(open(floor_site + "/contact/"))
     cities = doc.css("h3")
 
     cities.map do |name|
@@ -23,6 +24,19 @@ class FlooringGem::Scraper
     city_names.delete("\u00A0")
     city_names.delete("")
     city_names
+  end
+
+  def products
+    product_names = []
+    doc = Nokogiri::HTML(open(product_site))
+    products = doc.css("span.name-span")
+
+    products.map do |name|
+      product_names << name.text.strip
+    end
+
+    product_names
+
   end
 
 end
