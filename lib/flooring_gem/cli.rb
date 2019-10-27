685257096e7a1d8ab1f88c6b312188797c961cd3
Andrew Williams <1williamsaj@gmail.com>
2019-10-27 16:45:47 +0000

    updated menu

--- lib/flooring_gem/cli.rb
+++ lib/flooring_gem/cli.rb
@@ -16,6 +16,20 @@ class FlooringGem::CLI
     puts "4. Contact a Store"
     puts ""
     puts "Enter the number of the resource(1-4) you wish to access, or type 'exit' to close the program: "
+    user_choice = gets.strip
+
+    if user_choice == 'exit' || user_choice == 'Exit'
+      puts "Thanks for visiting! Come back soon!"
+      exit
+    elsif user_choice.to_i == 1
+      puts "Which store are you interested in?"
+    else
+      puts ""
+      puts "I'm sorry, that was an invalid selection. Please try again."
+      puts ""
+      menu
+    end
+
   end
 
 
