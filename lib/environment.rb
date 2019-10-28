e17420cc60f199d829cc0308f510f974d0c7bdd8
Andrew Williams <1williamsaj@gmail.com>
2019-10-28 23:18:44 +0000

    tooling

--- lib/environment.rb
+++ lib/environment.rb
@@ -6,6 +6,8 @@ require_relative "./flooring_gem/version"
 require_relative "./flooring_gem/cli"
 require_relative "./flooring_gem/scraper"
 require_relative "./flooring_gem/inventory"
+require_relative "./flooring_gem/site_scraper"
+require_relative "./flooring_gem/city"
 
 module FlooringGem
   class Error < StandardError; end
--- lib/flooring_gem/site_scraper.rb
+++ lib/flooring_gem/site_scraper.rb
@@ -1,8 +1,14 @@
 class FlooringGem::SiteScraper
 
   def self.scrape_cities
-    doc = 
+    doc = Nokogiri::HTML(open('https://themasterscraft.com/contact/'))
+    data = doc.css("h3")
+    cities = []
 
+    data.each do |city|
+      name = city.text.strip
+      puts name
+    end
 
   end
 
