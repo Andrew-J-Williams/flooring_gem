3372f324d1ae2a905b3712c5b21c2548627daf07
Andrew Williams <1williamsaj@gmail.com>
2019-10-28 22:44:25 +0000

    added city class

---
+++ lib/flooring_gem/city.rb
@@ -0,0 +1,16 @@
+class FlooringGem::City
+
+  attr_accessor :name, :inventory
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @inventory = []
+  end
+
+  def self.all
+    
+  end
+
+
+end
