df3777d571ddbe88dea318fa23eaf9a0e61897b7
Andrew Williams <1williamsaj@gmail.com>
2019-10-30 03:03:12 +0000

    building menu functionality

--- lib/flooring_gem/cli.rb
+++ lib/flooring_gem/cli.rb
@@ -8,7 +8,7 @@ class FlooringGem::CLI
     while @user_choice != 'exit' || @user_choice != 'Exit'
       cities
       cities_list
-      get_user_city
+      user_selection
   end
 
   def cities
@@ -20,79 +20,39 @@ class FlooringGem::CLI
     @city.each.with_index(1) do |city, index|
       puts "#{index}. #{city.name}"
     end
+
     puts ""
     puts "To view a store's inventory, enter the number next to the city name: "
   end
 
-  def get_user_city
+  def user_selection
    city_choice = gets.strip.to_i
 
-   if valid_input(city_choice, @city)
-     show_inventory_for(city_choice)
+   if city_choice > @city.length
+     puts ""
+     puts "Sorry, but you entered #{city_choice}. We only have 12 locations. Please enter a different value to continue: "
+     user_selection
+   elsif city_choice <= 0
+     puts ""
+     puts "Let's stay positive here! Try entering another number greater than 0 to continue:"
+     user_selection
+   else
+      show_inventory_for(city_choice)
    end
   end
 
- def valid_input(input, data)
-   input.to_i <= data.length && input.to_i > 0
- end
-
-  def show_inventory_for(chosen_city)
-    city = @city[chosen_city - 1]
+  def show_inventory_for(city_choice)
+    city = @city[city_choice - 1]
     city.check_inventory
-    puts "Here are products for #{city.name}"
-    city.inventory.each.with_index(1) do |product, idx|
-      puts "#{idx}. #{product.name} =#{product.price} (#{product.quantity})"
+    puts ""
+    puts "Below is an inventory count for our #{city.name} store. Pricing is listed in paratheses: "
+    city.inventory.each.with_index(1) do |product, index|
+      puts "#{index}. #{product.name} - #{product.quantity} (#{product.price})"
     end
 
+    puts ""
   end
 
-
-
-  def menu
-    puts ""
-    puts "Wood Flooring Resources:"
-    puts ""
-    puts "1. Check Store Inventory"
-    puts "2. Installation Guides"
-    puts "3. Calculate Square Footage"
-    puts "4. Contact a Store"
-    puts ""
-    puts "Enter the number of the resource(1-4) you wish to access, or type 'exit' to close the program: "
-    user_choice = gets.strip
-
-    if user_choice == 'exit' || user_choice == 'Exit'
-      puts ""
-      puts "Thanks for visiting! Come back soon!"
-      exit
-    elsif user_choice.to_i == 1
-      choose_city
-    else
-      puts ""
-      puts "I'm sorry, that was an invalid selection. Please try again."
-      puts ""
-      menu
-    end
+  def repeat?
 
   end
-
-  def choose_city
-      FlooringGem::Inventory.new.find_products
-
-      puts ""
-      puts "Scroll up to see the results for your inventory search."
-      puts "Do you wish to search again? (Y/N):"
-
-      while user_choice = gets.strip
-        if user_choice == "Y" || user_choice == "y"
-          choose_city
-        elsif user_choice == "N" || user_choice == "n"
-          break
-        else
-          puts "Sorry, that's not an option! Try again."
-        end
-      end
-      menu
-  end
-
-
-end
